7994963c13b3914b995ec7a68735cc5fe922e8f8
minefled <florian-martin@stembal.de>
2022-10-14 18:19:09 +0200

    Setup frontend

---
+++ frontend/src/lib/components/Head.svelte
@@ -0,0 +1,97 @@
+<div class="head">
+    <div class="logo">
+        <div class="icon">
+            <img src="/favicon.png" alt="">
+        </div>
+    
+        <div class="label">
+            Educatic
+        </div>
+    </div>
+
+    <div class="create-assignment">
+        <button>Add Assignment</button>
+    </div>
+</div>
+
+<style lang="scss">
+    .head {
+        width: 100%;
+
+        display: flex;
+        flex-direction: row;
+        align-content: center;
+        justify-content: space-between;
+
+        margin-bottom: 12px;
+    }
+
+    div.logo {
+        height: 70px;
+
+        margin-top: 12px;
+        margin-left: 16px;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .icon {
+        height: 70px;
+        width: 70px;
+
+        //background-color: #ffffff;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        img {
+            width: 80%;
+        }
+    }
+
+    .label {
+        font-family: "Kanit";
+        font-size: 30px;
+        font-weight: 700;
+
+        margin-left: 0.5em;
+
+        color: #ffffff;
+        text-shadow: 0px 0px 8px #00000044;
+    }
+
+    div.create-assignment {
+        height: 70px;
+
+        margin-top: 12px;
+        margin-right: 16px;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        button {
+            font-family: "Inter";
+            font-size: 16px;
+            font-weight: 800;
+            color: #ffffff;
+
+            background: rgb(32,116,218);
+            background: linear-gradient(142deg, rgba(32,116,218,1) 0%, rgba(111,84,255,1) 100%); 
+            border: none;
+            border-radius: 0.3em;
+
+            padding: 0.5em 1.2em;
+
+            transition: 0.4s;
+
+            -webkit-user-select: none;   
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+    }
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+    <title>Assigner</title>
+
+    <link rel="icon" href="/favicon.png">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Kanit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+</svelte:head>
+
+<script lang="ts">
+    import Head from "$lib/components/Head.svelte";
+</script>
+
+<div class="app">
+    <Head />
+</div>
+
+<style lang="scss">
+    .app {
+        position: fixed;
+        top: 0;
+        left: 0;
+
+        width: 100vw;
+        height: 100vh;
+
+        background-color: #14161b;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
